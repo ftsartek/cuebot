925d5cf64e8854ef0e3dbb18848bd6f8cfb031ff
Jordan Russell <jordanbrussell@gmail.com>
2022-07-07 08:41:24 +1200

    Fixed handling of existing users on restart

--- main.py
+++ main.py
@@ -205,14 +205,18 @@ async def check_voicechannel(server):
         await queue_channel.purge(limit=100, check=own_messages)
         voice_queue = bot.get_channel(server.voice_channel)
         members = [bot.get_user(key) for key in voice_queue.voice_states]
+        print(members)
         for queued_user in session.query(Queue).filter_by(server_id=server.id).all():
+            # Members who were previously in queue and still are
             if queued_user.member_id in [member.id for member in members]:
                 update_member(bot.get_user(queued_user.member_id), server)
                 add_queue(queued_user.member_id, server.id)
-                members.remove(queued_user.member_id)
+                members.remove(bot.get_user(queued_user.member_id))
+            # Members who were in queue but now are not
             elif queued_user.member_id not in members and queued_user.timeout_start is None:
                 remove_queue(queued_user.member_id, server.id)
-                members.remove(queued_user.member_id)
+                members.remove(bot.get_user(queued_user.member_id))
+        # Members who were not previously in queue but have joined
         for member in members:
             update_member(member, server)
             add_queue(member.id, server.id)
