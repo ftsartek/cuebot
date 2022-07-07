a4bd1decfb35b91acf81cdb0e44f6348c70f54f5
Jordan Russell <jordanbrussell@gmail.com>
2022-07-08 11:43:43 +1200

    Improved formatting of queue info list

--- main.py
+++ main.py
@@ -394,7 +394,7 @@ async def full_queue_info(ctx: commands.Context) -> None:
         for item in related:
             if item.queue_count > 0:
                 queue_total = convert_seconds(item.queue_time)
-                printout = printout + f"{item.nick:25} {item.queue_count:>15} " \
+                printout = printout + f"{item.nick:25} {str(item.queue_count):15} " \
                                       f"{queue_total[0]}d {queue_total[1]}h {queue_total[2]}m {queue_total[3]}s\n"
                 if len(printout) > 1500:
                     printout = printout + "```"
